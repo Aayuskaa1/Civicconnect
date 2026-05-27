fa7f0ec6c3d3694e2b698fb4f3aa14aac66755fd
Aayuska Adhikari <adhikariaayuska78@gmail.com>
2026-05-27 15:29:57 +0545

    fix: refine login validation logic

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,28 @@
-import 'package:flutter/widgets.dart';
-import '/app.dart';
+import 'package:civic_connect/views/splash_view.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'screens/dashboard_screen.dart';
+import 'themes/app_theme.dart';
 
 void main() {
-  runApp(const App());
+  runApp(
+    // ProviderScope stores the state of all Riverpod providers
+    const ProviderScope(
+      child: MyApp(),
+    ),
+  );
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Civic Connect',
+      debugShowCheckedModeBanner: false,
+      theme: getApplicationTheme(),
+      home: const SplashView(),
+    );
+  }
 }
